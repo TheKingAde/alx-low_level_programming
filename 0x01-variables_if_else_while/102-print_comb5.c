dba077b7249ee029e02b0efcc1caf03111eee9ae
ADE <theking-ade@outlook.com>
2023-06-18 04:10:05 -0700

    fix 13th programme

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (number1 = 0; number1 <= 98; number1++)
 	{
-	for (number2 = number1; number2 <= 99; number2++)
+	for (number2 = number1 + 1; number2 <= 99; number2++)
 	{
 		putchar((number1 / 10) + '0');
 		putchar((number1 % 10) + '0');
