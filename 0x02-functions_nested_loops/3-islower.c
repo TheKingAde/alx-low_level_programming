e54b1b50354c927d9045e4cfcf3d8aa5bbfa7259
ADE <theking-ade@outlook.com>
2023-06-20 23:52:16 -0700

    add 4th programme and update readme

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,24 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point
+ * _islower - Entry point
+ *@c: The character to be checked
  *
- * Return: Always 0 (Success)
+ * Return: 1 if lowercase and 0 if otherwise (Success)
  */
+
+int _islower(int c)
+{
+	int result;
+
+	if (_islower(c))
+	{
+		_putchar('1');
+		result = 1;
+	} else
+	{
+		_putchar('0');
+		result = 0;
+	}
+	return (result);
+}
