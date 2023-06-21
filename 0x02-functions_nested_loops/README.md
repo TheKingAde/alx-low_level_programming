41d08c83be905bf4f8b7a72723564ace6fe77cb3
ADE <theking-ade@outlook.com>
2023-06-20 18:12:37 -0700

    add 2nd programme and update readme

--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,4 +1,4 @@
 This is the readme for C functions and nested loops programmes
 
-1st programme - prints '_putchar' using putchar
-
+1st programme - prints '_putchar'_ using putchar
+2md programme - prints the alphabet, in lowercase, followed by a new line
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_alphabets(void)
+{
+	int alphabets;
+
+	for (alphabets = 97; alphabets <= 122; alphabets++)
+	{
+	putchar(alphabets);
+	}
+	putchar('\n');
+}
+#endif
