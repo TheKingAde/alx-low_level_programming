b12bdf0681047ac62cd5fb10bd59daee711c795b
ADE <theking-ade@outlook.com>
2023-06-24 04:46:49 -0700

    add 4th programme

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry
+ *
+ * Return: 1 if success and 0 otherwise
+ */
+void print_numbers(void)
+{
+	int result;
+
+	for (result = 0; result <= 0; result++)
+	{
+		_putchar(result + '0');
+	}
+	_putchar('\n');
+}
