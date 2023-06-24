38f82339d117b2d40a0f149c9e446ea4b11533e6
ADE <theking-ade@outlook.com>
2023-06-24 04:31:05 -0700

    update 2-mul file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - writes the character c to stdout
+ * @a: number
+ * @b: number
+ *
+ * Return: On success computed number.
+ * On error, -1 is returned
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
