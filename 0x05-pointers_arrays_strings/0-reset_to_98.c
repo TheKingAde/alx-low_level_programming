35c6c0ce294df877e347a71e0b7cc11aa0c2acda
ADE <theking-ade@outlook.com>
2023-06-26 16:51:59 -0700

    add first function file and update readme

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry
+ * @n: an integer
+ *
+ *Return - 1 if success and 0 there wise
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+Tis is the readme for the alx C pointers and arrays project
+
+1st programme - takes a pointer to an int as parameter and updates the value it points to to 98
