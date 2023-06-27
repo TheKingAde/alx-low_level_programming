e7def2230283a1dddea4a6ba80adedfd79cc2053
ADE <theking-ade@outlook.com>
2023-06-26 23:49:22 -0700

    fix seventh function

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,7 +11,11 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	while (str[i] != '\0' && str[i + 1] != '\0')
+	{
 		_putchar(str[i]);
+		_putchar(str[i + 1]);
+		i += 2;
+	}
 	_putchar('\n');
 }
