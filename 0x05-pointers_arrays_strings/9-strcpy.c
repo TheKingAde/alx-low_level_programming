7bdfb6dfc6974474a3c95f9623ade2ffb168d0ef
ADE <theking-ade@outlook.com>
2023-06-26 21:57:17 -0700

    add 10th function

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - entry
+ * @dest: destination
+ * @src: source
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
