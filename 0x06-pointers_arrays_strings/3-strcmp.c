567a960b42e6b760295f55a71f860ef063461599
ADE <theking-ade@outlook.com>
2023-06-29 20:23:30 -0700

    add 4th function

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - Entry
+ * @s1: string
+ * @s2: string
+ *
+ * Return: 1 if success otherwise if fail
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
