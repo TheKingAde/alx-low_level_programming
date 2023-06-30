f36508b7b25b83f87760030f579405e7d9ca0fe9
ADE <theking-ade@outlook.com>
2023-06-30 02:02:40 -0700

    add 10th function

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - Entry
+ * @n: integer
+ *
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n ==  0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar('0' + n % 10);
+}
