b51f2d54c132271a12fd96cfba1d0a987c13e517
ADE <theking-ade@outlook.com>
2023-07-11 16:54:19 -0700

    fix first func

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,13 +10,16 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *array = (char*)malloc(size * sizeof(char));
 	unsigned int i;
+	char *array;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
+
+	array = (char *)malloc(size * sizeof(char));
+
 	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
