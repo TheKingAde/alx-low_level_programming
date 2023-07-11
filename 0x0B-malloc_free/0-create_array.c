6a2cd9f7cbedd35a59bbc09d367c4fc24e1c485c
ADE <theking-ade@outlook.com>
2023-07-11 16:45:02 -0700

    add 1st func n update RM.md

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of char and initializes it with a char
+ * @size: size of array
+ * @c: char
+ *
+ * Return: pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array = (char*)malloc(size * sizeof(char));
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,3 @@
+This is a read me for the tasks in the alx malloc, free project
+=======================================================================================
+first func = creates an array of chars, and initializes it with a specific char.
