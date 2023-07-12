bdd7f5a3cba4dacc81aeb9d2b481c718f0bdf600
ADE <theking-ade@outlook.com>
2023-07-12 06:48:40 -0700

    add 5th func

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -6,14 +6,14 @@
  * @height: height
  *
  */
-void free_grid(in **grid, int height)
+void free_grid(int **grid, int height)
 {
+	int i;
+
 	if (grid == NULL || height <= 0)
 	{
 		return;
 	}
-	int i;
-
 	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
