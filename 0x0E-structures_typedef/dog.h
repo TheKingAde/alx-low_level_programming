0c4c078962d24a1e6e253f85cfc451f5df707fdf
ADE <theking-ade@outlook.com>
2023-07-17 22:13:07 -0700

    fix dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
-typedef struct dog Dog;
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+} Dog;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
