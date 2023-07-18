1b4d7d32b75110f3babdfec963a51f3640ebe36c
ADE <theking-ade@outlook.com>
2023-07-17 22:54:37 -0700

    add 2nd func

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
-
+/*
+ * typedef struct dog - holds some info
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: holds the name, age, and owner information of a dog.
+ *
+ */
 typedef struct dog
 {
 	char *name;
