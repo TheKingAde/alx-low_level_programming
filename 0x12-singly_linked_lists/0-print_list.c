08d87dc51573275a186604a5f9a4207abd9676ce
ADE <theking-ade@outlook.com>
2023-07-27 16:06:42 -0700

    add first func, updt RM, add lists.h file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list: prints a list of singly linked list
+ * @h: pointer
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t number = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		number++;
+	}
+	return (number);
+}
