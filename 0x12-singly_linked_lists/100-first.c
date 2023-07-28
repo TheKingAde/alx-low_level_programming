d40977c35732a7f1a3fe058a73789ae7c3a03a4e
ADE <theking-ade@outlook.com>
2023-07-27 19:34:50 -0700

    add 6th func

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+void print_before_main(void) __attribute__((constructor));
+
+/**
+ * print_before_main - prints before main func
+ */
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
