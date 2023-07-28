defe6d0fa0ead746e546eb705630355974569144
ADE <theking-ade@outlook.com>
2023-07-27 19:08:58 -0700

    update read me

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -17,32 +17,27 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	if (str == NULL)
 		return (NULL);
-
 	new_node = malloc(sizeof(list_t));
-
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-
 	len = 0;
 	while (str[len] != '\0')
 		len++;
-
-	new_node->str = malloc(sizeof(char) * (len + 1));;
+	new_node->str = malloc(sizeof(char) * (len + 1));
 	if (new_node->str == NULL)
 	{
 		free(new_node);
-		return(NULL);
+		return (NULL);
 	}
-
 	i = 0;
 	for (i = 0; i <= len; i++)
-	       new_node->str[i] = str[i];
-
+	{
+		new_node->str[i] = str[i];
+	}
 	new_node->len = len;
 	new_node->next = NULL;
-
 	if (*head == NULL)
 	{
 		*head = new_node;
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -2,7 +2,8 @@ this is the readme for the alx singly linked list project
 ===============================================================================================
 first func = prints all th elements in a list
 second func = returns the number of elements in a lsit
-third func = adds a new nodeat the top of a list
+third func = adds a new node at the top of a list
+fourth func = adds a new node at the end of the list
 
 ======
 note = main.c files are test files for the functions
