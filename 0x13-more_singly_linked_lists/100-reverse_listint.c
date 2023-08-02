cf355aaac3cbbccb07695b28f15f1549035fe028
ADE <theking-ade@outlook.com>
2023-08-01 21:22:21 -0700

    fix adv. rev. list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,5 +1,24 @@
 #include "lists.h"
 /**
+ * reverse_listint_recursive - reverse listint_t recursive
+ * @current: head's current position
+ *
+ * Return: Recursive
+ */
+listint_t *reverse_listint_recursive(listint_t *current, listint_t *prev)
+{
+	listint_t *next;
+
+	if (current == NULL)
+	{
+		return (prev);
+	}
+	next = current->next;
+	current->next = prev;
+
+	return (reverse_listint_recursive(next, current));
+}
+/**
  * reverse_listint - reverse a linked list
  * @head: pointer to a pointer to the head
  *
@@ -7,18 +26,11 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev;
-	listint_t *current;
-
-	prev = NULL;
-
-	while (*head != NULL)
+	if (*head == NULL)
 	{
-		current = (*head)->next;
-		(*head)->next = prev;
-		prev = *head;
-		*head = current;
+		return (NULL);
 	}
-	*head = prev;
+
+	*head = reverse_listint_recursive(*head, NULL);
 	return (*head);
 }
