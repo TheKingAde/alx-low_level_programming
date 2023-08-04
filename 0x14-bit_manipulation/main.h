61dd8211c7868accbda7536bf8d0210f61137734
ADE <theking-ade@outlook.com>
2023-08-03 20:03:42 -0700

    add get endianness function

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,4 +7,16 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+
+/**
+ * union - integer and char array
+ * @integer_value: integer value
+ * @bytes: array of bytes
+ */
+union my_union
+{
+    unsigned int integer_value;
+    unsigned char bytes[4];
+};
 #endif
