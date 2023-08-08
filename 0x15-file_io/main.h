343b79ca098fed37f672549cab2d93f1af291acc
ADE <theking-ade@outlook.com>
2023-08-07 21:38:45 -0700

    add 2nd func(create_file)

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,5 +7,6 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif
