51940180cfe70b085310a856a5b78bb1b8f9e3af
ADE <theking-ade@outlook.com>
2023-08-08 00:04:05 -0700

    add 4th func (cp.c)

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,6 +13,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	while (text_content != NULL && text_content[len] != '\0')
+		len++;
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
@@ -20,9 +22,6 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		while (text_content[len] != '\0')
-			len++;
-
 		if (write(fd, text_content, len) == -1)
 		{
 			close(fd);
