7100c9fe3d40cb524aae222eb39ce5f4ed6b9786
ADE <jeffersonmeffun@gmail.com>
2023-08-31 05:58:23 -0700

    fix 2nd function

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,12 +8,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	char c;
-
 	if (n > 1)
 		print_binary(n >> 1);
 
-	c = (n & 1) + '0';
-
-	return (write(STDOUT_FILENO, &c, 1));
+	_putchar((n & 1) + '0');
 }
