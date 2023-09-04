3ffa8690be9152b10eb98c228070ac1926e8e468
ADE <jeffersonmeffun@gmail.com>
2023-09-04 08:16:51 -0700

    add append_text_to_file function

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: the name of the file
+ * @text_content: the NULL terminated string to add at the end of the file
+ * Return: 1 on success and -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, len, res;
+	char *buffer;
+	int i;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+
+	len = 0;
+	while (text_content[len] != '\0')
+		len++;
+	buffer = malloc(len + 1);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (-1);
+	}
+	for (i = 0; i < len; i++)
+		buffer[i] = text_content[i];
+	buffer[len] = '\0';
+
+	res = write(fd, text_content, len);
+	if (res == -1)
+	{
+		close(fd);
+		free(buffer);
+		return (-1);
+	}
+
+	close(fd);
+	free(buffer);
+	return (1);
+}
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -2,3 +2,4 @@ this is the readme for the alx file input/output project tasks
 ======================================================================
 ssize_t read_textfile(const char *filename, size_t letters) = reads a text file and prints to the standard output
 int create_file(const char *filename, char *text_content) = creates a file
+int append_text_to_file(const char *filename, char *text_content) = appends text at the end of a file
